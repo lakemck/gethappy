919785b9086253338058291de0159fa411ea4958
Lake McKenna <lakemckenna@gmail.com>
2016-01-29 11:10:09 +1300

    updated title and description in template file

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,8 +6,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
+        <title>Get Happy</title>
+        <meta name="description" content="NZ's best happy hour and deal finder">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
