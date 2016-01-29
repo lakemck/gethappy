bb6f9fb4ca57bf267124e61ab558152d1df50203
Lake McKenna <lakemckenna@gmail.com>
2016-01-30 12:28:36 +1300

    Update select2.js

--- public/js/select2.js
+++ public/js/select2.js
@@ -1808,8 +1808,8 @@ S2.define('select2/selection/search',[
 
   Search.prototype.render = function (decorated) {
     var $search = $(
-      '<li class="select2-search select2-search--inline">' +
-        '<input class="select2-search__field" type="search" tabindex="-1"' +
+      '<li>' +
+        '<input tabindex="-1"' +
         ' autocomplete="off" autocorrect="off" autocapitalize="off"' +
         ' spellcheck="false" role="textbox" aria-autocomplete="list" />' +
       '</li>'
