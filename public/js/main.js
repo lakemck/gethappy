3010099dda8f0084bde7ee2233c422e117535466
Lake McKenna <lakemckenna@gmail.com>
2016-01-30 12:21:53 +1300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    // $(".select2-search, .select2-focusser").remove();
+    $(".select2-search, .select2-focusser, .select2-search--inline ").remove();
 
     $('.categorySelector, .distanceSelector').hide();
         
