b39c609cc2efaa78ff6c2b6743a2be7178b13c69
Lake McKenna <lakemckenna@gmail.com>
2016-01-30 12:33:01 +1300

    Update select2.js

--- public/js/select2.js
+++ public/js/select2.js
@@ -1811,7 +1811,7 @@ S2.define('select2/selection/search',[
       '<li>' +
         '<input tabindex="-1"' +
         ' autocomplete="off" autocorrect="off" autocapitalize="off"' +
-        ' spellcheck="false" role="textbox" aria-autocomplete="list" />' +
+        ' spellcheck="false" role="textbox" aria-autocomplete="list" style="display: none" />' +
       '</li>'
     );
 
