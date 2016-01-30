03438f3de1502c8d7334d4d62213274caa5ffede
Lake McKenna <lakemckenna@gmail.com>
2016-01-30 13:07:55 +1300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ $(function(){
 
     $(".select2-search, .select2-focusser, .select2-search--inline ").remove();
    
-   if(!$(li).hasClass(.select2-selection__choice)){ 
+   if(!$(li).hasClass("select2-selection__choice")){ 
     $(".placeHolderText").eq(0).text('category');
       $(".placeHolderText").eq(1).text('day');
 }
