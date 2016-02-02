bdf995f34cd3933f418ee31c5c554fa12de5473d
Lake McKenna <lakemckenna@gmail.com>
2016-02-03 08:37:03 +1300

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -81,8 +81,6 @@
 
     @yield('content')
 
-    <!-- Use yield for multiple things. eg if you want a footer in another page but not on the homepage you can yield('footer') here, then write different code under @section('footer') on the other page-->
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
@@ -91,18 +89,15 @@
 {!! HTML::script('js/infobubble.js'); !!}
 <script src="{{URL::to('js/select2.js')}}"></script>
 <script type="text/javascript" src="{{ URL::asset('js/main.js') }}"></script>
-// <script>
-//   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-//   ga('create', 'UA-73224940-1', 'auto');
-//   ga('send', 'pageview');
-// </script>
-
-
-<!--       </footer> -->
+  ga('create', 'UA-73224940-1', 'auto');
+  ga('send', 'pageview');
+</script>
     @yield('footer')
-    </body>
+</body>
 </html>
