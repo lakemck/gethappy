5e8cd4c027df346b1ed10b5fde29739b4161e6b5
Lake McKenna <lakemckenna@gmail.com>
2016-03-01 19:08:19 +1300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,38 +13,40 @@
       <div class="logoName">    
         <h1 class="siteTitle">GET HAPPY</h1>
         <h2>NZ's HAPPY HOUR/DEAL FINDER</h2>
+        <h2>UNDER MAINTENANCE BACK TOMORROW</h2>
       </div>
     @if (Session::has('flash_message'))
       <div class="noResultsFlash">{{ Session::get('flash_message')}}</div>
     @endif
   </div>
-  <div class="row searchBoxes show">
+  
+  <!--<div class="row searchBoxes show">-->
 
-    {!! Form::open(['method' => 'GET', 'id' => 'searchForm', 'name' => 'searchForm', 'route' => 'articles_path']) !!}
+  <!--  {!! Form::open(['method' => 'GET', 'id' => 'searchForm', 'name' => 'searchForm', 'route' => 'articles_path']) !!}-->
 
-      {!! Form::input('search', 'q', null, ['placeholder' => 'ENTER A LOCATION', 'class' => 'locationSearch', 'id' => 'searchInput'])!!}
-      <div class="searchFormErrors" style="display: none">Enter Address</div>
-    {!! $errors->first('search','<p class="error">:message</p>')!!}
+  <!--    {!! Form::input('search', 'q', null, ['placeholder' => 'ENTER A LOCATION', 'class' => 'locationSearch', 'id' => 'searchInput'])!!}-->
+  <!--    <div class="searchFormErrors" style="display: none">Enter Address</div>-->
+  <!--  {!! $errors->first('search','<p class="error">:message</p>')!!}-->
 
-      {!! Form::hidden('lat', null, ['id' => 'lat'])!!}
-      {!! Form::hidden('lng', null, ['id' => 'lng'])!!}
+  <!--    {!! Form::hidden('lat', null, ['id' => 'lat'])!!}-->
+  <!--    {!! Form::hidden('lng', null, ['id' => 'lng'])!!}-->
 
-      <div id="geolocationButton" class="geolocationSearch"><span>or</span> Use My Location <i class="fa fa-location-arrow"></i></div>
+  <!--    <div id="geolocationButton" class="geolocationSearch"><span>or</span> Use My Location <i class="fa fa-location-arrow"></i></div>-->
 
-      <div class="hiddenFields totesHidden">
-        {!! Form::select('categoryList[]', $categories, null, ['placeholder' => 'TYPE', 'class' => 'categorySelector', 'id' => 'categoryList', 'multiple']) !!}
-      {!! $errors->first('category','<p class="error">:message</p>')!!}
-        {!! Form::select('dayList[]', $days, null, ['class' => 'distanceSelector', 'id' => 'dayList', 'multiple']) !!}
-      {!! $errors->first('day','<p class="error">:message</p>')!!}
+  <!--    <div class="hiddenFields totesHidden">-->
+  <!--      {!! Form::select('categoryList[]', $categories, null, ['placeholder' => 'TYPE', 'class' => 'categorySelector', 'id' => 'categoryList', 'multiple']) !!}-->
+  <!--    {!! $errors->first('category','<p class="error">:message</p>')!!}-->
+  <!--      {!! Form::select('dayList[]', $days, null, ['class' => 'distanceSelector', 'id' => 'dayList', 'multiple']) !!}-->
+  <!--    {!! $errors->first('day','<p class="error">:message</p>')!!}-->
 
-      {!! Form::submit('MAKE ME HAPPY', array('id' => 'submitButton', 'class' => 'searchSubmit')) !!}
+  <!--    {!! Form::submit('MAKE ME HAPPY', array('id' => 'submitButton', 'class' => 'searchSubmit')) !!}-->
     
-      {!! Form::close() !!}
-    </div>
-  </div>
-  <div class="row developmentMode">
-    <p>*Just Auckland & Wellington atm</h4>
-  </div>
+  <!--    {!! Form::close() !!}-->
+  <!--  </div>-->
+  <!--</div>-->
+  <!--<div class="row developmentMode">-->
+  <!--  <p>*Just Auckland & Wellington atm</h4>-->
+  <!--</div>-->
 
 
      <!-- Example row of columns -->
