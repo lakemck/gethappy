83470cab423b9a3a4a65cd66631e9bb3bb74bd48
Lake McKenna <lakemckenna@gmail.com>
2016-03-07 10:48:05 +1300

    Update ArticlesController.php

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -229,14 +229,10 @@ public function index(Request $request)
     }
 
     $article->update($request->all());
-    //   $dayID = Deal::all()->lists('dealname', 'dayID');
-    //   $dayID = $dayID->toArray();
 
-    // // $dealnames = $article->deals->dealname;
-    // dd($dayID);
 
     for($i = 0; $i < sizeof($request->input('dealname')); $i++) {
-
+        $article = Article::findOrFail($id);
 
         $article->deals->where('dayID',($i + 1))->first()->dealname = $request->input('dealname')[$i];
         $article->deals->where('dayID',($i + 1))->first()->save();
