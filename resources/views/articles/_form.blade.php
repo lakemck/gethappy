bcaf2b696388227d04b2c9d5c35f7c3a2abf0cb2
Lake McKenna <lakemckenna@gmail.com>
2016-03-07 08:20:22 +1300

    Update _form.blade.php

--- resources/views/articles/_form.blade.php
+++ resources/views/articles/_form.blade.php
@@ -34,6 +34,62 @@
   
 </div>
 
+
+@foreach ($article->deals as $deal)
+         
+        @if($deal->dayID == '1')
+
+        <div class="form-group">
+            {!! Form::label('dealname','Monday') !!}
+            {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '1']) !!}
+        </div>
+        @endif
+
+        @if($deal->dayID == '2' )
+        <div class="form-group">
+            {!! Form::label('dealname','Tuesday') !!}
+            {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '2']) !!}
+        </div>
+        @endif
+        @if($deal->dayID == '3' )
+        <div class="form-group">
+            {!! Form::label('dealname','Wednesday') !!}
+            {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '3']) !!}
+        </div>
+        @endif
+
+@if($deal->dayID == '4')
+<div class="form-group">
+    {!! Form::label('dealname','Thursday') !!}
+    {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '4']) !!}
+</div>
+
+@endif
+
+
+@if($deal->dayID == '5' )
+<div class="form-group">
+    {!! Form::label('dealname','Friday') !!}
+    {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '5']) !!}
+</div>
+@endif
+
+@if($deal->dayID == '6' )
+<div class="form-group">
+    {!! Form::label('dealname','Saturday') !!}
+    {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '6']) !!}
+</div>
+@endif
+
+@if($deal->dayID == '7' )
+<div class="form-group">
+    {!! Form::label('dealname','Sunday') !!}
+    {!! Form::text('dealname[]', $deal->dealname, null, ['class' => 'form-control', 'id' => '7']) !!}
+</div>
+@endif
+
+@endforeach
+
 <div class="form-group">
     {!! Form::label('address','ADDRESS') !!}
     {!! Form::text('address', null, ['class' => 'form-control']) !!}
